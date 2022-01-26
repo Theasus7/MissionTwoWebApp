371a4377b1a33f46a6ddb90b20f9befde31e8629
Theasus7 <anderson.taylor98@gmail.com>
2022-01-26 10:22:04 -0700

    Changes

---
+++ MissionOneWebApp/Controllers/CoolBeansController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MissionOneWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MissionOneWebApp.Controllers
+{
+    public class CoolBeansController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult gradeCalculator ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult gradeCalculator (GradeCalculator model)
+        {
+            return View();
+        }
+    }
+}
---
+++ MissionOneWebApp/Models/GradeCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MissionOneWebApp.Models
+{
+    public class GradeCalculator
+    {
+        [Required]
+        [Range(0, 100)]
+        public int Ass { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int Pro { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int Qui { get; set; }
+
+        [Required]
+        public int Exa { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int INT { get; set; }
+
+    }
+}
